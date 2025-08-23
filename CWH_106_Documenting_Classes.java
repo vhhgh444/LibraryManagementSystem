5ece04fcd8fa91e638bbbc8df125b141129c01e9
Anuj Samanta <anujsamanta6@gmail.com>
2025-08-23 19:16:07 +0530

    Add files via upload

---
+++ CWH_106_Documenting_Classes.java
@@ -0,0 +1,22 @@
+package co.company;
+
+/**
+ * This class is to demonstrate what javadocs is and how it is used in the java industry
+ * @author Anuj
+ * @version 1
+ * @since 2024
+ * @see <a href="https://docs.oracle.com/en/java/javase/14/docs/api/index.html">Java Docs</a>
+ */
+
+
+
+public class CWH_106_Documenting_Classes {
+    public void add(int a,int b){
+        System.out.println("The sum is: "+a+b);
+    }
+    public static void main(String[] args) {
+
+        System.out.println("This is my main method");
+
+    }
+}
